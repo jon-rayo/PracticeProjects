628f65a7f0d1930dd834ef802e583da3eddd78a8
Jon Rayo <jonathangrayo@lewis.edu>
2020-09-21 21:22:26 -0500

    Did loops examples today

---
+++ PayCheckWLoops/src/PayCheckWLoops.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class PayCheckWLoops {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		double hours, rate, gross;
+		String doAgain;
+		do {
+			System.out.print("Enter hours and rate: ");
+			hours = sc.nextDouble();
+			rate = sc.nextDouble();
+			gross = hours * rate;
+			System.out.printf("Gross = $%.2f.\n", gross);
+			System.out.print("Again? ");
+			doAgain = sc.next(); 
+		} while (doAgain.equalsIgnoreCase("y"));
+		System.out.println("Thank you for using this program.");
+	}
+	
+}
